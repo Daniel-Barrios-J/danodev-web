c65454af59641e8ac1070b626c7248034586c2a0
Daniel Barrios J <daniel.bj.rd@gmail.com>
2022-10-13 01:24:07 -0500

    actualizacion portfolio y menu desplegable

--- src/components/pure/Header.jsx
+++ src/components/pure/Header.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import { NavLink } from 'react-router-dom';
 import '../../styles/header.css'
 
 const Header = () => {
+
+  const obtenerAncho = ()=> {
+    let widthSize = document.documentElement.clientWidth;
+    setWidth(widthSize);
+    console.log(widthSize)
+  }
+
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    obtenerAncho()
+  }, []);
+  
+
+  console.log(width)
+
   return (
     <header className='header'>
         <NavLink className='inicio-container' to={'/'}>
           <h3 className='inicio-title'>DanoDev</h3>
         </NavLink>
         <nav className='nav-container'>
-          <ul className='list-nav'>
+          <img alt='menu' src="https://img.icons8.com/nolan/64/menu.png"/>
+          <ul className={width > 640 ? 'list-nav' : 'menu-mobile'}>
             <NavLink className='list-item' to={'/'}>Home</NavLink>
             <NavLink className='list-item' to={'/about'}>About</NavLink>
             <NavLink className='list-item' to={'/contact'}>Contact</NavLink>            
