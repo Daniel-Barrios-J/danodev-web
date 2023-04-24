c50205f54d38a145c353431d6fc7cc60ee749318
Daniel Barrios J <daniel.bj.rd@gmail.com>
2023-04-24 01:28:14 -0600

    add: button portfolio and styles

---
+++ src/utils/skills.js
@@ -0,0 +1,60 @@
+import { imgUrls } from "./imagesURLS"
+export const skills = [
+  {
+    tech:'React',
+    img: imgUrls.REACT
+  },
+  {
+    tech:'Redux',
+    img: imgUrls.REDUX
+  },
+  {
+    tech:'Node',
+    img: imgUrls.NODE
+  },
+  {
+    tech:'Express',
+    img: imgUrls.EXPRESS
+  },
+  {
+    tech:'Sass',
+    img: imgUrls.SASS
+  },
+  {
+    tech:'GIT',
+    img: imgUrls.GIT
+  },
+  {
+    tech:'LIT',
+    img: imgUrls.LIT
+  },
+  {
+    tech:'MongoDb',
+    img: imgUrls.MONGODB
+  },
+  {
+    tech:'Firebase',
+    img: imgUrls.FIREBASE
+  },
+  {
+    tech:'JavaScript',
+    img: imgUrls.JAVASCRIPT
+  },
+  {
+    tech:'GitHub',
+    img: imgUrls.GITHUB
+  },
+  {
+    tech:'HTML',
+    img: imgUrls.HTML
+  },
+  {
+    tech:'CSS',
+    img: imgUrls.CSS
+  },
+  {
+    tech:'MUI',
+    img: imgUrls.MUI
+  },
+
+]
